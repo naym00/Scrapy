619e1cb72a6410a48c54b93eb2322389eb669c97
naym00 <naymhsain00@gmail.com>
2022-01-10 19:48:02 +0600

    commit--2

--- postscrape/spiders/post_spider.py
+++ postscrape/spiders/post_spider.py
@@ -1,44 +1,71 @@
 import scrapy
+import json
 
 class Post_Spider(scrapy.Spider):
     name = "posts"
     page = 1
 
     start_urls = [
-        'https://www.kayak.co.in/Hyderabad-Hotels.7297.hotel.ksp',
+        'https://www.kayak.co.in/Hyderabad-Hotels.7297.hotel.ksp'
     ]
     def parse(self, response):
-        # print()
-        # print()
-        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")   
-        # # print(response.css('p::text').getall())
-        hotel_name = response.css('div.soom-description a span::text')[0].get()
-        hotel_rating = response.css('div.soom-description .soom-rating-wrapper span::text')[0].get().split(" ")[0]
-        hotel_str = response.css('div.soom-description .soom-price-section span::text')[0].get()
-        hotel_price = response.css('div.soom-description .soom-price::text')[0].get()
-        hotel1 = response.css('div.soom-freebies')[0].get().split("yRv1-text")[1]
-        hotel2 = response.css('div.soom-freebies')[0].get().split("yRv1-text")[2]
-        hotel3 = response.css('div.soom-freebies')[0].get().split("yRv1-text")[3]
-
-        h1 = hotel1.split("<");
-        h2 = hotel2.split("<");
-        h3 = hotel3.split("<");        
-
-        print(h1[0][2: len(h1[0])])
-        print(h2[0][2: len(h2[0])])
-        print(h3[0][2: len(h3[0])])
-        # hotel_1 = hotel[0].response.css('div.soom-freebie').get()
-        #  .soom-freebie .yRv1-text::text
-        # li_2 = li.response.css('.ct-div-block')[0]
-        print(hotel_name)
-        print(hotel_rating)
-        print(hotel_str)
-        print(hotel_price)
-        # print(hotel_1)
+        print()
         print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        # print()
-        # print()   
+        list_data = response.css('div.c44F-content .c44F-item')
+        # if len(list_data)!=0:
+        #     for i in range(0, len(list_data), 1):
+        #         print(list_data[i].css('div.soom .soom-photo-wrapper .soom-photo::attr(src)').get())
+        #         print(list_data[i].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-name span::text').get())
+        #         print(list_data[i].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-rating-wrapper span::text').get())
+        #         print(list_data[i].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-price-section .soom-neighborhood::text').get())
+        #         print(list_data[i].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-price::text').get())
+        #         list_lower = list_data[i].css('div.soom .soom-content-wrapper .soom-freebies-section .soom-freebies .soom-freebie span::text').getall()
+        #         print(list_lower)
+        #         print()
+        # else:
+        #     list_data = response.css('div.Hotels-Region-LatestHotelDeals .deals-cities-grid div')
+        #     for i in range(0, len(list_data), 1):
+        #         print(list_data[i].css('div.deal-city a .deal-city-img .deal-city-name::text').get())
+        #         print()
+        
+        
+
+        str_data = response.css('#__R9_HYDRATE_DATA__::text').get()
+        json_data = json.loads(str_data)
+        best_hotels = json_data['serverData']['contentState']['bestHotelsModel']['bestHotels']
+        for i in range(0, len(best_hotels), 1):
+            print(best_hotels[i]['imageUrl'].split('?')[0])
+            print()
+        
+        landmark_hotels = json_data['serverData']['contentState']['bestHotelsModel']['landmarkHotels']
+        for i in range(0, len(landmark_hotels), 1):
+            print(landmark_hotels[i]['imageUrl'].split('?')[0])
+            print()
+
+        # print("{0} {1}".format(len(list_data), len(best_hotels)+len(landmark_hotels)))
+
+
+
+        # print(len(response.css('div.c44F-content .c44F-item')))
+        # list_data = response.css('div.c44F-content .c44F-item')
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # list_data[0].css('div.soom .soom-photo-wrapper') For Image
+        # list_data[0].css('div.soom .soom-content-wrapper .soom-description-wrapper') Upper Section
+        # list_data[0].css('div.soom .soom-content-wrapper .soom-freebies-section') Lower Section
+        # print(list_data[0].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-name span::text').get())
+        # print(list_data[0].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-rating-wrapper span::text').get())
+        # print(list_data[0].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-description .soom-price-section .soom-neighborhood::text').get())
+        # print(list_data[0].css('div.soom .soom-content-wrapper .soom-description-wrapper .soom-price::text').get())
+
+        # list_lower = list_data[0].css('div.soom .soom-content-wrapper .soom-freebies-section .soom-freebies .soom-freebie')
+        # print(list_lower[0].css('span::text').get())
+        # print(list_lower[1].css('span::text').get())
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # if str(type(list_data[i].css('div.soom .soom-photo-wrapper .soom-photo::attr(src)').get())) == "<class 'str'>":
+        # print("Hidden Image- > " + list_data[i].css('div.soom .soom-photo-wrapper .soom-photo::attr(alt)').get())
+        
         # filename = 'posts-%s.html' % self.page
         # self.page += 1 
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
+        
